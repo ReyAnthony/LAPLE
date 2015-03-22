e9166fc690082d13ff91a4a765c47fe83f7bc99d
Gabriel ZAAFRANI <gabriel.zaafrani@gmail.com>
2015-03-22 20:17:05 +0100

    Added annotation parser POC

---
+++ POC/ParseurAnnotation/src/parseurannotation/AnnotationImplem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parseurannotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author zaafranigabriel
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AnnotationImplem {
+    public String name();
+    public String value();
+    public String content();
+    
+}
+
