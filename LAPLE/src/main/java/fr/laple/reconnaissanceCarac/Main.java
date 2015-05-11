a15b32069c91dce5aece2177a6417f20e058e220
christian <chrisebongue@hotmail.fr>
2015-05-12 01:11:24 +0200

    suppression des anciens fichiers java. Ajout des nouveaux fichiers Draw.java, Fenetre.java, Main.java,  Point.java.  Changements effectués:  suppression de valider. java. Le boutons valider et reset sont directement gérés dans fenetre.java. Ainsi on a la possibilité d'effacer et de dessiner autant de fois que l'on souhaite

---
+++ LAPLE/src/main/java/fr/laple/reconnaissanceCarac/Main.java
@@ -0,0 +1,12 @@
+package reconnaissanceCarac;
+
+public class Main {
+
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		Fenetre fen=new Fenetre();
+		fen.setVisible(true);
+	}
+
+}
