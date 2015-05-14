f1453b6cbb158634c494592ded2b63965ce1252d
christian <chrisebongue@hotmail.fr>
2015-05-14 18:45:52 +0200

    ajout des fichiers DbAcessor.java, Local.java, Web.java

--- LAPLE/src/main/java/fr/laple/jdbc/src/Main.java
+++ LAPLE/src/main/java/fr/laple/jdbc/src/Main.java
@@ -5,44 +5,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import management.DbAccessor;
+import management.Local;
+import management.Web;
 
+/* A faire: Caster le arraylist de local et web
+ ajouter d'autres variables de classe
+ Bref ce n'était qu'un test, beaucoup de choses à modifier
+ */
 public class Main {
-
-	//CTRL + SHIFT + O pour générer les imports
 	
-	  public static void main(String[] args) {      
-		Connection conn=null;
-	    try {
-	      Class.forName( "com.mysql.jdbc.Driver" );
-	      System.out.println("Driver O.K.");
-
-	      String url = "jdbc:mysql://localhost:3306/test";
-	      String user = "root";
-	      String passwd = "masters";
-
-	      conn = DriverManager.getConnection(url, user, passwd);
-	      System.out.println("Connexion effective !");
-	      Statement statement = conn.createStatement();
-	      /* Exécution d'une requête de lecture */
-	      ResultSet resultat = statement.executeQuery( "SELECT * FROM Animal;" );
-	      while ( resultat.next() ) {
-	    	    int id = resultat.getInt( "id" );
-	    	    String espece = resultat.getString( "espece" );
-	    	    String nom = resultat.getString( "nom" );
-
-	    	    System.out.println(id + '|' + espece + '|' + nom);
-	    	}
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    }finally {
-	        if ( conn != null )
-	            try {
-	                /* Fermeture de la connexion */
-	            	conn.close();
-	            } catch ( SQLException ignore ) {
-	                /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
-	            }
-	    }      
+	  public static void main(String[] args) throws SQLException, ClassNotFoundException {      
+		Local bdd= new Local("jdbc:mysql://localhost:3306/test", "root", "masters");
+		Web bdd2= new Web("root", "masters");
+		bdd.get();
+		bdd2.get();
+		if ( DbAccessor.getConn()!= null )
+			try {
+            /* Fermeture de la connexion */
+        	DbAccessor.getConn().close();
+			} catch ( SQLException ignore ) {
+            /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
+        }
 	  }
 
 }
