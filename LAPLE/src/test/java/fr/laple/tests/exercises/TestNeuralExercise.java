b7452197f406df2a8451cc63393417668ed0c240
Anthony Rey <anthony.rey5@gmail.com>
2015-05-29 01:00:58 +0200

    Created a test for an actually working char recognition, totally by hand instead of using the buggy neuroph IDE. Also added res for thoses

--- LAPLE/src/test/java/fr/laple/tests/exercises/TestNeuralExercise.java
+++ LAPLE/src/test/java/fr/laple/tests/exercises/TestNeuralExercise.java
@@ -8,15 +8,78 @@ import fr.laple.language.Symbol;
 import fr.laple.language.SymbolContainer;
 import org.junit.Assert;
 import org.junit.Test;
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.imgrec.ColorMode;
+import org.neuroph.imgrec.FractionRgbData;
+import org.neuroph.imgrec.ImageUtilities;
+import org.neuroph.imgrec.image.Dimension;
+import org.neuroph.imgrec.image.ImageJ2SE;
+import org.neuroph.nnet.learning.BackPropagation;
+import org.neuroph.ocr.OcrHelper;
+import org.neuroph.ocr.OcrPlugin;
+import org.neuroph.util.TransferFunctionType;
 
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by anthonyrey on 28/05/2015.
  */
 public class TestNeuralExercise {
 
+    @Test
+    public void buildNeuralNet()
+    {
+
+        Dimension dimension = new Dimension(130,130);
+        ColorMode colorMode = ColorMode.BLACK_AND_WHITE;
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("a");
+        labels.add("e");
+        labels.add("u");
+        labels.add("i");
+        labels.add("o");
+
+        ArrayList<Integer> layerNeuronsCount = new ArrayList<>();
+
+        TransferFunctionType transfertFuncType = TransferFunctionType.SIGMOID;
+
+        NeuralNetwork net = OcrHelper.createNewNeuralNetwork("neuron", dimension, colorMode, labels,
+                layerNeuronsCount, transfertFuncType);
+
+        File trainingSetPath = new File(getClass().getResource("/symbols/hira_training").getPath());
+        HashMap<String, BufferedImage> images = new HashMap<>();
+
+        for(File f : trainingSetPath.listFiles())
+        {
+            try {
+                images.put(f.getName().replace(".png", ""),
+                        ImageUtilities.resizeImage(ImageIO.read(f), dimension.getWidth(), dimension.getHeight()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Map<String, FractionRgbData> fractions = ImageUtilities.getFractionRgbDataForImages(images);
+        DataSet set = OcrHelper.createBlackAndWhiteTrainingSet(labels, fractions);
+        net.learn(set, new BackPropagation());
+
+        BufferedImage testImage = null;
+        try {
+            testImage = ImageIO.read(getClass().getResource("/symbols/a_hira.bmp"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        OcrPlugin plugin = (OcrPlugin)net.getPlugin(OcrPlugin.class);
+        System.out.println(plugin.recognizeCharacterProbabilities(new ImageJ2SE(testImage)));
+
+    }
 
     @Test
     public void newExercise(){
