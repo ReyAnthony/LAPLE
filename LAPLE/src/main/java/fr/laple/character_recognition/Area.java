3d04527d67c8b6353ad35f138414c7600aea0e18
christian <chrisebongue@hotmail.fr>
2015-06-04 01:31:19 +0200

    update character_recognition. Add in class area method PointToPixel

---
+++ LAPLE/src/main/java/fr/laple/character_recognition/Area.java
@@ -0,0 +1,61 @@
+package character_recognition;
+
+import java.util.ArrayList;
+
+public class Area {
+	public static final int VIDE=0;
+	public static final int PLEIN=1;
+	public static final int X=8;
+	public static final int Y=8;
+	
+	private int[] matrix;
+	
+	public Area() {
+		this.matrix = new int[X * Y];
+		for(int i=0; i<matrix.length;i++)
+			matrix[i]=VIDE;
+	}
+	
+	
+	public int[] getMatrix() {
+		return matrix;
+	}
+	/**
+	 * Convert the coordinated of point to pixel
+	 * @param point	List of points. Result of  mouse dragged
+	 * @param posX	Origin x of landmark
+	 * @param posY	Origin y of landmark
+	 * @param cX the side x of one square
+	 * @param cY the side y of one square
+	 *  @exception if one coordinate of point does'nt in area 
+	 */
+	public void pointToPixel(ArrayList<Point> point, int posX, int posY, int cX, int cY){
+		int pixelX;
+		int pixelY;
+		for(Point p: point){
+			pixelX = (p.getMouseX()-posX)/cX;
+			pixelY=(p.getMouseY()-posY)/cY;
+			try{
+				if(pixelY==0){
+					matrix[pixelX]=PLEIN;
+				}
+				else{
+					matrix[pixelY*8 + pixelX]=PLEIN;
+				}
+			}catch(ArrayIndexOutOfBoundsException e){
+				System.out.println(e);
+			}
+		}
+	}
+	@Override
+	public String toString() {
+		String str="{";
+		for(int p : this.matrix){
+	        
+			str= str + ";" + p;
+	    }
+		str=str+"}";
+		return str;
+	}
+	
+}
---
+++ LAPLE/src/main/java/fr/laple/character_recognition/Point.java
@@ -0,0 +1,67 @@
+package character_recognition;
+
+
+public class Point{
+	int mouseX;
+	int mouseY;
+	int mouseSize;
+
+	public Point(){
+		this.mouseX = -1;
+		this.mouseY = -1;
+		this.mouseSize = -1;
+	}
+	public Point(int mouseX, int mouseY, int mouseSize) {
+		this.mouseX = mouseX;
+		this.mouseY = mouseY;
+		this.mouseSize = mouseSize;
+	}
+	public int getMouseX() {
+		return mouseX;
+	}
+	public void setMouseX(int mouseX) {
+		this.mouseX = mouseX;
+	}
+	public int getMouseY() {
+		return mouseY;
+	}
+	public void setMouseY(int mouseY) {
+		this.mouseY = mouseY;
+	}
+	public int getMouseSize() {
+		return mouseSize;
+	}
+	public void setMouseSize(int mouseSize) {
+		this.mouseSize = mouseSize;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + mouseSize;
+		result = prime * result + mouseX;
+		result = prime * result + mouseY;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Point)){
+			return false;
+		}
+		
+		Point other = (Point) obj;
+		if (this.mouseSize != other.mouseSize)
+			return false;
+		if ((this.mouseX <other.mouseX-10) || (this.mouseX >other.mouseX+10))
+			return false;
+		if ((this.mouseY <other.mouseY-10) || (this.mouseY >other.mouseY+10))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Point [mouseX=" + mouseX + " mouseY=" + mouseY + "] \n";
+	}
+	
+
+}
