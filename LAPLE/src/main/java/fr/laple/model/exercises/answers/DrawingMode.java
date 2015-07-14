390212c067ebcb3f0ccf2f8e3bd137b1e2d91373
Anthony Rey <anthony.rey5@gmail.com>
2015-07-14 11:10:11 +0200

    Neuroph removal

--- LAPLE/src/main/java/fr/laple/model/exercises/answers/DrawingMode.java
+++ LAPLE/src/main/java/fr/laple/model/exercises/answers/DrawingMode.java
@@ -1,8 +1,6 @@
 package fr.laple.model.exercises.answers;
 
 import fr.laple.controller.exercises.AbstractExerciseController;
-import fr.laple.extensions.languages.japanese.neural.NeuralExerciseSolver;
-import fr.laple.model.exercises.ExModeUserLangTranscriptLang;
 import fr.laple.model.exercises.IExerciseMode;
 import fr.laple.model.exercises.IExerciseSolver;
 import fr.laple.view.exercises.AbstractExerciseView;
@@ -12,9 +10,10 @@ import fr.laple.view.exercises.AbstractExerciseView;
  */
 public class DrawingMode extends AbstractAnswerMode {
 
+    //TODO
     @Override
     public IExerciseSolver getSolver() {
-        return new NeuralExerciseSolver();
+        return null;
     }
 
     @Override
@@ -25,7 +24,9 @@ public class DrawingMode extends AbstractAnswerMode {
     @Override
     public boolean testIfAnswerAndQuestionAreCompatible(IExerciseMode mode) {
 
-        return mode instanceof ExModeUserLangTranscriptLang;
+        return false;
+        //TOOD
+        //return mode instanceof ExModeUserLangTranscriptLang;
 
     }
     @Override
